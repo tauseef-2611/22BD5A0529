e317001deab20b25a1100da32a6de3accb0da973
tauseef-2611 <138360590+tauseef-2611@users.noreply.github.com>
2024-05-30 19:40:35 +0530

    first_push

---
+++ top_products/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Add this line
+import Products from './components/Products';
+
+function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
